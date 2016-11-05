242826ec43a1cbea157fb980dc4e105b015db8d8
Jade Ella Abao <jcabao@users.noreply.github.com>
2016-11-05 13:37:05 +0800

    Update lab6problem1.cpp

--- lab6problem1.cpp
+++ lab6problem1.cpp
@@ -1,19 +1,21 @@
 //laboratory 6 problem1
 //abao jade ella
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-	
 
 int main(){
     FILE *fp;
-	fp = fopen("sample.txt", "w");
+	fp = fopen ("sample.txt", "w");
 	    
-    if(!fp){
+    if (!fp){
     cout << "Cannot open file";
-    system("pause");
-    exit(1);}
-	for(int i=65; i<91; i++){
-    fputc(i, fp);}    
-	fclose(fp);
-	return 0;
-	}
+    system ("pause");
+    exit(1);
+    }
+	for (int i=65; i<91; i++){
+    fputc (i, fp);
+	}    
+	fclose (fp);
+return 0;
+}
